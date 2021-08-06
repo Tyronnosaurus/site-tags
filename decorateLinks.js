7f3f43eb4ad3dc0d2932940ee6f1baeb1bdd9704
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2021-08-06 15:26:06 +0200

    Lots of refactoring (but no functionality changes)

--- decorateLinks.js
+++ decorateLinks.js
@@ -2,78 +2,94 @@
 decorateAllLinksInPage();   //Since this script gets loaded as a content script, this function gets executed whenever a website is loaded
 
 
-
+//Fetches all links in the page and decorates those that have a tag
 function decorateAllLinksInPage(){
     
     var linkNodes = document.links; //Get list of all links in the page
 
     for(var i=0; i<linkNodes.length; i++)
-        decorateLink(linkNodes[i]);
+        decorateLinkIfNecessary(linkNodes[i]);
 }
 
 
 
-function decorateLink(linkNode){
+//Given a single html link node, decorates it if it has a tag
+function decorateLinkIfNecessary(linkNode){
    
+    const url = normalizeUrl(linkNode.href);
+
     //Start an asynchronous query to retrieve any value under the 'url' key in local storage.
-    url = linkNode.href;
-    url = normalizeUrl(url);
+    browser.storage.local.get(url)
 
-    fetchQuery = browser.storage.local.get(url);
-
-    //fetchQuery is a promise. Using 'then()', we can pass 2 functions (success & fail) that will be executed when the query finishes.
+    //The function above is a promise. Using 'then()', we can pass 2 functions (success & fail) that will be executed when it finishes.
     //When the success function is executed, it will get passed as a parameter whatever was returned by browser.storage.local.get(url).
-    fetchQuery.then(
-
-        function(storedMap){            //When data is successfully fetched from storage, run this function.
-            onGot(storedMap, linkNode); //The anonymous function wraps another function. We need to do this to pass any extra argument not returned by browser.storage.local.get()
-        },
-
-        onError);                       //If fetching fails, run this function
+    .then(BuildFunction_DecideIfDecorationNeeded(linkNode) , onFetchError);
 
 }
 
 
 
-function onGot(storedMap, linkNode) {
-    //Succeded fetching local storage
-    //Returns a Map (aka Dictionary) with key:value pairs. Since we provided only one key, we get a Map with one pair.
-    //If the specified key didn't exist in local storage, value 'undefined' will be returned
 
-    //Get the value in the first pair of the Map
-    tagList = storedMap[Object.keys(storedMap)[0]];
+//Due to the way javascript's promises work, the function inside then() only gets passed whatever is returned by the local.get() (storedMap).
+//In order to pass extra parameters, we do this wrapping: a function that accepts any parameters and hardcodes them into another function that we return.
+function BuildFunction_DecideIfDecorationNeeded(linkNode){
 
-    if (typeof tagList == 'undefined')  return;   //No info for this url found in local storage
+    function DecideIfDecorationNeeded(storedMap){
+        //Succeded fetching local storage --> Returns a Map with key:value pairs.
+        //Since we provided only one key, we get a Map with one pair.
+        //If the specified key didn't exist in local storage, value 'undefined' will be returned
 
-    else if (tagList.includes("seen"))
-    {
-        ////Add icon after the link
+        //Get the value in the first pair of the Map
+        tagList = storedMap[Object.keys(storedMap)[0]];
 
-        //Create <img> node
-        var myImage = new Image(25, 20);
-
-        //Get icon's url
-        //Returns something like:  moz-extension://2c127fa4-62c7-7e4f-90e5-472b45eecfdc/icons/file.ext
-        //The long number is a randomly generated ID for every browser instance. This prevents fingerprinting
-        myImage.src = browser.runtime.getURL("icons/seen_20px.png");
-
-        //In the html, insert the image after the link (not inside) 
-        insertAfter(myImage , linkNode);
+        if (typeof tagList == 'undefined')  return;     //No info for this url found in local storage
+        else if (tagList.includes("seen"))              //Url had tag --> Append icon to link
+            decorateLinkNode(linkNode);
     }
 
+    return(DecideIfDecorationNeeded);   //Return the whole function
+
 }
 
 
-function onError(error) {
-    //Failed to retrieve value from local storage
+//Run when failed to retrieve value from local storage 
+function onFetchError(error) {
     //Note that not finding a value for a specified key is not an error. It just returns 'undefined'
     console.log(`Error: ${error}`);
 }
 
 
-//Inserts a new node after another existing node
-function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+
+
+
+
+//Decorate a link (append icon)
+function decorateLinkNode(linkNode){
+    //Create <img> node
+    var myImage = new Image(25, 20);
+    myImage.src = getResourcesRuntimeUrl("icons/seen_20px.png");
+
+    //In the html, insert the image after the link (not inside) 
+    insertAfter(linkNode, myImage);
 }
 
 
+
+//Get runtime url of a resource (a file included with the extension)
+function getResourcesRuntimeUrl(relativePath){
+    //relativePath is the file's relative path to manifest.json in the extension's folder structure.
+    //This is not really accessible by the browser during runtime.
+
+    //Instead, we need something like:  moz-extension://2c127fa4-62c7-7e4f-90e5-472b45eecfdc/icons/file.ext
+    //The long number is a randomly generated ID for every browser instance. This prevents fingerprinting.
+    
+    //The file must have been made accessible in manifest.json -> web_accessible_resources.
+    return( browser.runtime.getURL(relativePath) );
+}
+
+
+
+//Inserts a new html node after another existing node
+function insertAfter(existingNode, newNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
